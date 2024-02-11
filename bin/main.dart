8cc9d662bfa961ed745918ed2067c0e4b94aa633
Maham Jamil <jamilmaham944@gmail.com>
2024-02-12 00:51:53 +0500

    heelo

---
+++ bin/main.dart
@@ -0,0 +1,15 @@
+void main(){
+  print("hello dart");
+
+
+  var name="maham";
+  print(name);
+
+  bool a=true;
+  print(a);
+
+  var b = false;
+  print(b);
+  //used to know the datatype 
+  print(b.runtimeType);
+}
